51488d784ee2a90e86cfe45b399a2a6dd922713e
Nikita71 <76263258+Nikita7131@users.noreply.github.com>
2022-09-26 09:44:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(){
     SetConsoleCP(1251);
 
     printf("ctreated by: ");
-    printf("%s %s Ðå-22\n", D_Name, D_Surname);
+    printf("%s %s Ре-22\n", D_Name, D_Surname);
 
     while(1){
 
@@ -36,17 +36,13 @@ int main(){
      scanf("%s %s %d %d %d",&stud[i].Name, &stud[i].Surname, &stud[i].grade_1, &stud[i].grade_2, &stud[i].grade_3);
      stud[i].avg_grade = (stud[i].grade_1+stud[i].grade_2+stud[i].grade_3+3)/3;
      printf("result avarage: %s %s = %lf\n", stud[i].Name, stud[i].Surname, stud[i].avg_grade - 1);
-    
-      
      printf("\n");
-     if(stud[i].avg_grade > Top_Grade){ // дааа, можна було зробити сортирований список, но на годинку 2:00 і я хочу спати ....
+     if(stud[i].avg_grade > Top_Grade){ // дааа, можна було зробити сортирований список, но на годинку 1:5 і я хочу спати ...
       Top_Grade = stud[i].avg_grade;
      }
      printf(" Top : %lf",Top_Grade);
-      
-      
       i ++;
-      
+
     }
 
 }
